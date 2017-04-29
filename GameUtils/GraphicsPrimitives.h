b2c4598b83715683a42876161009dfd3a00d0b33
unknown <victordreynolds@gmail.com>
2017-04-28 22:33:23 -0700

    first attempt at home made blit

--- GameUtils/GraphicsPrimitives.h
+++ GameUtils/GraphicsPrimitives.h
@@ -43,10 +43,27 @@ struct PIXMAP
 	{
 		memcpy(dest->pixels, pixels, GetSize());
 	}
+	
 	void Blit(PIXMAP * dest, int x, int y)
 	{
-		
-		memcpy(dest->pixels, pixels, GetSize());
+		int srcX = 0,srcY = 0;
+		int width = ((x + w) > dest.w) ? dest.w : w;
+		int height = ((y + h) > dest.h) ? dest.h : h;
+		int row = 0;
+
+		for (unsigned int uiV = y; uiV < (srcY+height); ++uiV)
+		{
+			// reset coordinate for each row
+			pucPixel = &dest->pixels[(uiV * dest.w + srcX) * sizeof(RGB)];
+				
+			// *pfrgbaPixel++ = ColorFloatRGBA::ConvertFromRGBA8(pucPixel[0], pucPixel[1],pucPixel[2], pucPixel[3]);
+			
+			//memcpy(dest->pixels, pixels, GetSize());
+			//memcpy( &dst[dstIdx], &src[srcIdx], numElementsToCopy * sizeof( Element ) );
+			memcpy(pucPixel, &pixels[row], width * sizeof( RGB ) );
+			row += w;
+			
+		}
 	}
 	void Clear()
 	{
@@ -75,4 +92,64 @@ struct ScreenText
 			delete[] text;
 	}*/
 };
+void SourceImage::Read(int a_iPixelX, int a_iPixelY)
+	{
+		
+		int iBitsPerPixel;
+		//if stb_iamge is available, only use it to load files other than png
+		char *fileExt = strrchr(m_pstrFilename, '.');
 
+		if (strcmp(fileExt, ".png") != 0)
+		{
+			paucPixels = stbi_load(m_pstrFilename, &iWidth, &iHeight, &iBitsPerPixel, 4);
+
+			if (paucPixels == nullptr)
+			{
+				printf("stb_image error %s\n", stbi_failure_reason());
+				assert(0);
+				exit(1);
+			}
+		}
+#endif
+
+
+		//the pixel cords for the top left corner of the block
+		int iBlockX = 0;
+		int iBlockY = 0;
+		if (a_iPixelX > -1 && a_iPixelY > -1)
+		{
+			// in 1 block mode, we basically will have an img thats 4x4
+			m_uiWidth = 4;
+			m_uiHeight = 4;
+
+			if(a_iPixelX > iWidth)
+				a_iPixelX = iWidth;
+			if (a_iPixelY > iHeight)
+				a_iPixelY = iHeight;
+
+			// remove the bottom 2 bits to get the block coordinates 
+			iBlockX = (a_iPixelX & 0xFFFFFFFC);
+			iBlockY = (a_iPixelY & 0xFFFFFFFC);
+		}
+		else
+		{
+			m_uiWidth = iWidth;
+			m_uiHeight = iHeight;
+		}
+
+		m_pafrgbaPixels = new ColorFloatRGBA[m_uiWidth * m_uiHeight];
+		assert(m_pafrgbaPixels);
+
+		int iBytesPerPixel = bool16BitImage ? 8 : 4;
+		unsigned char *pucPixel;	// = &paucPixels[(iBlockY * iWidth + iBlockX) * iBytesPerPixel];
+		ColorFloatRGBA *pfrgbaPixel = m_pafrgbaPixels;
+
+		// convert pixels from RGBA* to ColorFloatRGBA
+		
+
+#if USE_STB_IMAGE_LOAD
+		stbi_image_free(paucPixels);
+#else
+		free(paucPixels);
+#endif
+	}
