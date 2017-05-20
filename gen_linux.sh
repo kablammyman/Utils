519f9c043f6094a015edd892fd624381e7899832
kablammyman <victordreynolds@gmail.com>
2017-05-19 23:25:00 -0700

    now i can build debug or relase builds in linux and gcc

--- gen_linux.sh
+++ gen_linux.sh
@@ -1,7 +1,11 @@
 echo export CC=/usr/bin/clang
 echo export CXX=/usr/bin/clang++
 mkdir -p build_linux
+mkdir -p x64/Debug
+mkdir -p x64/Release
 cd build_linux
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+#cmake -DCMAKE_BUILD_TYPE=Release ..
+#cmake ..
 cd ..
 
